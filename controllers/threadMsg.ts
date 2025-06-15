e23692fbbc2195c31a9aa862321bf3d0debb2c3d
JannaDev-py <orlandojannael@gmail.com>
2025-06-14 21:10:26 -0600

    getAll msgs endpoint

--- controllers/threadMsg.ts
+++ controllers/threadMsg.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from 'express'
+import { connection } from '../database/connect'
+import { ThreadMsgModel } from '../models/threadMsg'
 
 export const ThreadMsgController = {
-  getAll: async function (req: Request, res: Response) {},
+  getAll: async function (req: Request, res: Response) {
+    const threadId = req.body.threadId
+    const response = await ThreadMsgModel.getAll(threadId, await connection)
+    res.send(response)
+  },
+
   createMsg: async function (req: Request, res: Response) {},
   updateMsg: async function (req: Request, res: Response) {},
   deleteMsg: async function (req: Request, res: Response) {}

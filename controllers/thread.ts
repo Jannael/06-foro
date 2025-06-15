0e5968e52664db25c3bef4ce4142fc942565c93a
JannaDev-py <orlandojannael@gmail.com>
2025-06-14 20:53:33 -0600

    getAll endpoint

--- controllers/thread.ts
+++ controllers/thread.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from 'express'
+import { ThreadModel } from '../models/thread'
+import { connection } from '../database/connect'
 
 export const ThreadController = {
-  getAll: function (req: Request, res: Response) {
+  getAll: async function (req: Request, res: Response) {
+    const response = await ThreadModel.getAll(await connection)
+    res.send(response)
   },
 
   createThread: function (req: Request, res: Response) {
